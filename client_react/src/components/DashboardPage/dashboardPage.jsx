09af4bd3b1933086756d0b7dfc27abcf5bab5eb3
Shay Bushary <48178609+Shaybush@users.noreply.github.com>
2023-11-16 22:53:05 +0200

    Bugfix (#5)
    * delete unnecessary folders and files
    
    * asc-9 - fix duplicated messages and handle space
    
    * bugfix - small changes
    
    ---------
    
    Co-authored-by: Shay Bushary <shay.bushary@1touch.io>

--- client_react/src/components/DashboardPage/dashboardPage.jsx
+++ client_react/src/components/DashboardPage/dashboardPage.jsx
@@ -3,7 +3,7 @@ import DropdownButton from 'react-bootstrap/esm/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { googleLogout } from "@react-oauth/google";
 import Socket from '../socket';
-import mapIcon from "../../assets/icons/map.png"
+import mapIcon from "../../assets/icons/map.png";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = ({ existUser, setExistUser, setIsLogin }) => {
@@ -12,23 +12,23 @@ const Dashboard = ({ existUser, setExistUser, setIsLogin }) => {
   const logOut = () => {
     googleLogout();
     localStorage.removeItem("userData");
-    setExistUser(null)
+    setExistUser(null);
     setIsLogin(false);
-    navigate("/")
+    navigate("/");
   };
 
   return (
     <div className="text-end">
       <div className="d-flex justify-content-lg-end">
-        <button onClick={()=>{navigate("/map")}} className="border-0 bg-transparent">
-        <img src={mapIcon} className="rounded-circle me-1" style={{height:"100px"}}/>
+        <button onClick={() => navigate("map")} className="border-0 bg-transparent">
+          <img src={mapIcon} className="rounded-circle me-1" width={50} />
         </button>
-            {/* profile avatar */}
-      <DropdownButton id="dropdown-button" title={<img src={existUser.picture} className="rounded-circle me-5" />}>
-        <Dropdown.Item onClick={() => logOut()}>
-          LogOut
-        </Dropdown.Item>
-      </DropdownButton>
+        {/* profile avatar */}
+        <DropdownButton id="dropdown-button" title={<img src={existUser.picture} className="rounded-circle me-5" />}>
+          <Dropdown.Item onClick={() => logOut()}>
+            LogOut
+          </Dropdown.Item>
+        </DropdownButton>
       </div>
       {/* chat socket */}
       <Socket existUser={existUser} />
--- client_react/src/components/Loader/loader.jsx
+++ client_react/src/components/Loader/loader.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import { Rings } from 'react-loader-spinner';
 
 const LoaderSpinner = ({ load = true, height, width, color = "#97aef8" }) => {
-    return (
-        <Rings
-            height={height}
-            width={width}
-            color={color}
-            radius="9"
-            wrapperStyle={{}}
-            wrapperclassName=""
-            visible={load}
-            ariaLabel="rings-loading"
-        />
-    );
+    return <div>Loading</div>;
 };
 
 export default LoaderSpinner;
